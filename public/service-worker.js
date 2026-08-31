617a6d9ed76d7f1514e5b307d9edcd10624ea6b2
Keyur Gohil <133516013+KeyurGohilUK@users.noreply.github.com>
2026-08-31 15:23:58 +0100

    Bump service worker cache to 0.20.0

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "urbanfox-shell";
-const CACHE_VERSION = "0.19.0";
+const CACHE_VERSION = "0.20.0";
 const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VERSION}`;
 const APP_SHELL = [
   "./",
