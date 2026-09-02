97be6552c260ca33d414e89467e1bfe3e1af51fd
Keyur Gohil <133516013+KeyurGohilUK@users.noreply.github.com>
2026-09-02 17:12:57 +0100

    Handle current address deletion safely

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "urbanfox-shell";
-const CACHE_VERSION = "3.13.3";
+const CACHE_VERSION = "3.14.0";
 const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VERSION}`;
 const APP_SHELL = [
   "./",
