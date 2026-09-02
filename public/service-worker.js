493bf29ee211a4212767ad2b5ab1dc92125f1432
Keyur Gohil <133516013+KeyurGohilUK@users.noreply.github.com>
2026-09-02 13:58:39 +0100

    Update public/service-worker.js for Address History readiness

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "urbanfox-shell";
-const CACHE_VERSION = "3.10.3";
+const CACHE_VERSION = "3.11.0";
 const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VERSION}`;
 const APP_SHELL = [
   "./",
