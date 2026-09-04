d8f162c8b8a8ba3852767da3a98a2c2005c523f3
Keyur Gohil <133516013+KeyurGohilUK@users.noreply.github.com>
2026-09-04 18:59:03 +0100

    Update 3.25.1 cache version

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "urbanfox-shell";
-const CACHE_VERSION = "3.25.0";
+const CACHE_VERSION = "3.25.1";
 const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VERSION}`;
 const APP_SHELL = [
   "./",
