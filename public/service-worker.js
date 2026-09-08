78f8236750f64cfd0ecf01cac10ce1cae21b1033
Keyur Gohil <133516013+KeyurGohilUK@users.noreply.github.com>
2026-09-08 10:04:36 +0100

    Refresh cache for permission history UI

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "urbanfox-shell";
-const CACHE_VERSION = "3.25.15";
+const CACHE_VERSION = "3.25.16";
 const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VERSION}`;
 const APP_SHELL = [
   "./",
